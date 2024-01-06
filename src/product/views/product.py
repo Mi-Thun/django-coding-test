beaa472c221fa575ad676ed071bed0d703ec38a1
Mithun <mithun@sgcsoft.net>
2024-01-06 16:47:49 +0600

    list view done

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,7 +1,9 @@
 from django.views import generic
 
-from product.models import Variant
-
+from django.views import View
+from django.shortcuts import render
+from product.models import Product, ProductVariant, ProductVariantPrice, Variant
+from django.db.models import Q
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
@@ -12,3 +14,28 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+class ProductListView(View):
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = {'products': Product.objects.all()}
+        for product in context['products']:
+            product.variants = ProductVariant.objects.filter(product=product)
+            for variant in product.variants:
+                variant.prices = ProductVariantPrice.objects.filter(
+                    Q(product_variant_one_id=variant.id) |
+                    Q(product_variant_two_id=variant.id) |
+                    Q(product_variant_three_id=variant.id),
+                    product=product
+                )
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
+
+
+
+
+
